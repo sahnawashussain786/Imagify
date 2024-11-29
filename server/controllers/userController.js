26dcb4005a154e00666a70c286ec2406add473bf
Sahnawas Hussain <142085109+sahnawashussain786@users.noreply.github.com>
2024-11-29 11:31:00 +0530

    working on user credits functionality

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -55,4 +55,20 @@ const loginUser = async (req, res) => {
   }
 };
 
-export {registerUser, loginUser}
+const userCredits = async (req, res) => {
+  try {
+    const { userId } = req.body;
+
+    const user = await userModel.findById(userId);
+    res.json({
+      success: true,
+      credits: user.creditBalance,
+      user: { name: user.name },
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.json({success:true, message: error.message})
+  }
+};
+
+export { registerUser, loginUser };
