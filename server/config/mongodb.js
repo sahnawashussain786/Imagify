0ae3570bcbcce3b64dde3d1c57affdc20a085f79
Sahnawas Hussain <142085109+sahnawashussain786@users.noreply.github.com>
2026-01-13 18:52:51 +0530

    ..

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,12 +1,15 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const connectDB = async () => {
+  if (mongoose.connection.readyState >= 1) {
+    return;
+  }
 
-    mongoose.connection.on('connected',()=>{
-        console.log("Database Connected");
-    })
+  mongoose.connection.on("connected", () => {
+    console.log("Database Connected");
+  });
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/imagify`)
-}
+  await mongoose.connect(`${process.env.MONGODB_URI}/imagify`);
+};
 
 export default connectDB;
